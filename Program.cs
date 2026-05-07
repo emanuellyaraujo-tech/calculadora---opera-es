8c2ce479fe5020f8b87567934344059f2c94a46d
Emanuelly <emanuelly.d.araujo@aluno.senai.br>
2026-05-07 14:13:08 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace git_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            int numero1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número:");
+                int numero2 = int.Parse(Console.ReadLine());
+            int resultado = numero1 + numero2;
+            Console.WriteLine("resultado: " + resultado);
+        }
+    }
+}
