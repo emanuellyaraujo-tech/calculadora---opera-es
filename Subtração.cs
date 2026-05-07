01ad326526d407eaa259cbb4a5f5b76d26d81983
Emanuelly <emanuelly.d.araujo@aluno.senai.br>
2026-05-07 14:28:50 -0300

    Add files via upload

---
+++ Subtração.cs
@@ -0,0 +1,11 @@
+﻿namespace git_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int resultado = numero1 - numero2;
+            Console.WriteLine("a subtração é : " + resultado);
+        }
+    }
+}
